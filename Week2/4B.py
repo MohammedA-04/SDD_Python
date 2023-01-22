48ca130725f020d66309ea3ad1af21ed7144eb18
MohammedA-04 <116843561+MohammedA-04@users.noreply.github.com>
2023-01-22 15:36:09 +0000

    Delete Week2 directory

--- Week2/4B.py
+++
@@ -1,7 +0,0 @@
-Mass = float(input(" Hi User, Can you input the Mass of object [In KG] so we can determines it's energy?"))
-
-LightSpeed = 3 * 10**8
-print(">>>",LightSpeed, "Km/Sec is Speed of light")
-
-Energy = Mass * LightSpeed
-print("Answer = ","Energy of your Mass Object is : ", Energy, "Joules")
